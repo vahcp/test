61463518bd9bf35b7d18be7ffa7b44c1c4dd9dc6
vahcp <vahcp>
2022-10-18 15:52:24 +0900

    edit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,14 +19,21 @@ class PostController extends Controller
     {
         return view('posts/create');
     }
-    public function show(Post $post)
-    {
-        return view('posts/show')->with(['post' => $post]);
-    }
     public function store(PostRequest $request, Post $post)
     {
         $input = $request['post'];
         $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+    
         return redirect('/posts/' . $post->id);
     }
 
